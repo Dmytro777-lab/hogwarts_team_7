3fc090f7d2c1ccf3b5ac6d626a99a8062f0ca5be
Serhii <s.yakovenko@hubmee.com>
2024-11-08 21:18:23 +0200

    init-2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hogwarts_team_7
+
 Master of Sience Final Project
+
+start : python3 -m main
--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-from data_handler import load_data, save_data
-from commands_handler import listen_commands
-from users import Users
-from notes import Notes
-
-
 def main():
-    book = load_data("users.pkl", Users())
-    notes = load_data("notes.pkl", Notes())
-
-    def on_exit(book, notes):
-        save_data(book, "users.pkl")
-        save_data(notes, "notes.pkl")
-
-    listen_commands(book, notes, on_exit)
+    pass
 
 
 if __name__ == "__main__":
