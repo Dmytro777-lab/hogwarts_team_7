1caaf6a72aa0763813fbb7130abbfdb4dc9de738
Serhii <s.yakovenko@hubmee.com>
2024-11-10 18:04:42 +0200

    add ENUM for search options

--- hogwarts/hogwarts_notes/search.py
+++ hogwarts/hogwarts_notes/search.py
@@ -3,3 +3,15 @@ from enum import Enum
 
 class SearchBy(Enum):
     TITLE = "title"
+    TAGS = "tags"
+
+
+class SortBy(Enum):
+    TAGS = "tags"
+    CREATED_AT = "created_at"
+    MODIFIED_AT = "modified_at"
+
+
+class SortOrder(Enum):
+    ASC = "ascending"
+    DESC = "descending"
